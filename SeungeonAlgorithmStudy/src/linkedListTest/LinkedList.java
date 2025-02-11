8b1ad36e3f88feacedcd71aae355530706ad71e0
jaemyungchoi95 <woaud130@gmail.com>
2025-02-11 18:06:22 +0900

    커밋 메시지 (변경 사항에 대한 설명)

--- SeungeonAlgorithmStudy/src/linkedListTest/LinkedList.java
+++ SeungeonAlgorithmStudy/src/linkedListTest/LinkedList.java
@@ -1,23 +1,48 @@
 package linkedListTest;
 
 public class LinkedList implements ILinkedList {
+	public Node head; // 리스트의 시작 노드
+	public Node tail; // 리스트의 마지막 노드
+	
+	public LinkedList() {
+		this.head = new Node(); // 더미 head 노드 생성
+		this.tail = new Node(); // 더미 tail 노드 생성
+		head.next = tail; // head의 다음 노드를 tail로 설정
+		tail.prev = head; // tail의 이전 노드를 head로 설정
+	}
 
 	@Override
 	public void add(int index, Node node) {
+		// 만약 첫번째 값일 경우 (다음
+		if (this.head.next == null) {
+			Node nd = new Node(index, null, null);
+			this.head.next = nd;
+			this.tail.prev = nd;
+			return ;
+		}
 		
+		// 두번째 이후의 경우
+		Node nd = new Node(index, null, this.next.next);
+		this.head.prev.next = nd;
+		this.head.next = nd;
 		
 	}
 
 	@Override
 	public void remove(int value) {
-		
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void printAll() {
-		
+		Node 
 		
 	}
+
+
+
+
+
 
 }
--- SeungeonAlgorithmStudy/src/linkedListTest/Node.java
+++ SeungeonAlgorithmStudy/src/linkedListTest/Node.java
@@ -1,11 +1,30 @@
 package linkedListTest;
 
 public class Node {
-	int value;
-	Node prev;
-	Node next;
+	int value; // 저장할 데이터
+	Node prev; // 이전 데이터
+	Node next; // 다음 데이터
 	
+	// 기본 생성자
 	public Node() {
-	
+		this.prev = null;
+		this.next = null;
 	}
+
+	// 값만 받는 경우
+	public Node(int value) {
+		this.value = value;
+		this.prev = null;
+		this.next = null;
+	}
+	
+	// 전체 값을 받는 경우
+	public Node(int value, Node prev, Node next) {
+		super();
+		this.value = value;
+		this.prev = prev;
+		this.next = next;
+	}
+
 }
+
