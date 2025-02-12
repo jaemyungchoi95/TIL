dabb31c8a965cf1368ae26281453631c2eb3775d
jaemyungchoi95 <woaud130@gmail.com>
2025-02-13 07:48:32 +0900

    전체 파일 재정규화 및 불필요한 파일 삭제

--- Day03_0212/src/SWEA/회문2_1216/Solution.java
+++ Day03_0212/src/SWEA/회문2_1216/Solution.java
@@ -1,5 +1,35 @@
 package SWEA.회문2_1216;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
 
-}
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		// 해당 문제의 테스트케이스는 총 10개이다
+		for(int test_case = 1; test_case <= 1; test_case++) {
+		int tNum = sc.nextInt(); // 테스트케이스 번호
+		sc.nextLine(); // 개행 문자 제거
+		String[][] arr = new String[100][100]; // 글자판 2차원 배열
+		
+		for (int i = 0; i < 100; i++) {
+			String temp = sc.nextLine();
+			for (int j = 0; j < 100; j++) {
+				arr[i][j] = Character.toString(temp.charAt(j));
+			}
+		}
+		
+		for(int i = 0; i < 100; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		
+		
+		// 정답 출력
+//		System.out.printf("#%d %d", tNum, );
+			
+		} // 테스트케이스 끝
+		
+	} // main 끝
+	
+} // 클래스 끝
