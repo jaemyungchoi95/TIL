1530e361c4b255250f710bc65b6c29304cffb696
jaemyungchoi95 <woaud130@gmail.com>
2025-02-17 17:49:23 +0900

    버스...

--- IM준비/src/SWEA/삼성시의_버스_노선_6485/Solution.java
+++ IM준비/src/SWEA/삼성시의_버스_노선_6485/Solution.java
@@ -12,6 +12,7 @@ public class Solution {
 			int N = sc.nextInt(); // 버스 노선의 개수
 			int[][] lineArr = new int[N][]; // 버스노선의 개수를 담아줄 배열
 			StringBuilder sb = new StringBuilder();
+			int max = 0;
 			
 			// 노선을 운행하는 버스 노선을 받아준다
 			for (int i = 0; i < N; i++) {
@@ -22,21 +23,37 @@ public class Solution {
 			}
 			
 			int P = sc.nextInt(); // 정류장의 수를 입력받는다
+			int[] pArr = new int[P+1]; // 정류장 번호를 받아줄 배열 선언
 			// C1 ~ Cp 까지의 수 입력받는다
-			for (int i = 0; i < P; i++) {
-				sc.nextInt();
+			for (int i = 1; i <= P; i++) {
+				int tmp = sc.nextInt();
+				pArr[i] = tmp;
 			}
 			
-			int[] arr = new int[P+1];
+//			System.out.println(Arrays.toString(pArr));
+			
+			for (int i : pArr) {
+				if (i > max) {
+					max = i;
+				}
+			}
+			
+//			System.out.println(max);
+			
+			int[] arr = new int[max+1];
 			
 			for (int i = 0; i < N; i++) {
 				for (int j = lineArr[i][0]; j <= lineArr[i][1]; j++) {
-					arr[j]++;
+					if (j == pArr[j]) {
+						arr[j]++;
+					}
 				}
 			}
 			
 			for (int i = 1; i < arr.length; i++) {
-				sb.append(" ").append(arr[i]);
+				if (arr[i] != 0) {
+					sb.append(" ").append(arr[i]);
+				}
 			}
 			
 			// 정답 출력
