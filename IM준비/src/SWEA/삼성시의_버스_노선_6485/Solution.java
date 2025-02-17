e22c043808a8f794274aa90445707cab4e463ea1
jaemyungchoi95 <woaud130@gmail.com>
2025-02-17 17:34:13 +0900

    추가 update

---
+++ IM준비/src/SWEA/삼성시의_버스_노선_6485/Solution.java
@@ -0,0 +1,49 @@
+package SWEA.삼성시의_버스_노선_6485;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt(); // 버스 노선의 개수
+			int[][] lineArr = new int[N][]; // 버스노선의 개수를 담아줄 배열
+			StringBuilder sb = new StringBuilder();
+			
+			// 노선을 운행하는 버스 노선을 받아준다
+			for (int i = 0; i < N; i++) {
+				int aLine = sc.nextInt(); // A노선 번호
+				int bLine = sc.nextInt(); // B노선 번호
+				
+				lineArr[i] = new int[] {aLine, bLine};
+			}
+			
+			int P = sc.nextInt(); // 정류장의 수를 입력받는다
+			// C1 ~ Cp 까지의 수 입력받는다
+			for (int i = 0; i < P; i++) {
+				sc.nextInt();
+			}
+			
+			int[] arr = new int[P+1];
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = lineArr[i][0]; j <= lineArr[i][1]; j++) {
+					arr[j]++;
+				}
+			}
+			
+			for (int i = 1; i < arr.length; i++) {
+				sb.append(" ").append(arr[i]);
+			}
+			
+			// 정답 출력
+			System.out.printf("#%d%s\n", test_case, sb);
+
+		} // 테스트케이스 끝
+		
+	} // main 끝
+    
+} // 클래스 끝
